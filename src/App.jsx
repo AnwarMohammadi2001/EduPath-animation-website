731d437a17ea2022e9af7e4b90e8b3b0aae7e62b
Anwar Mohammadi <anwarmohammadi1390@gmail.com>
2025-08-18 16:27:59 +0430

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
